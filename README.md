0f930aefe374b326897d503a99cb433fe18eb4a9
Matthew H. Irby <matt.irby@outlook.com>
2023-07-27 08:27:00 -0400

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# PR Labels on Push
+
+A Github action that extracts labels from the most recent push and makes them available to other actions. Labels are available as step outputs and environment variables that you can use in later steps in your action.
+
+## How do I use this?
+
+Perhaps you have a test that you only want to run when PR label `release:master` is set. Your workflow should look like this:
+
+```yaml
+jobs:
+  test:
+    steps:
+      - name: Get PR labels
+        id: pr-labels
+        uses: irby/get-labels-on-push@main
+
+      # GITHUB_PR_LABEL_RELEASE_MASTER was set by pr-labels action
+      - run: |
+          if [ -n "$GITHUB_PR_LABEL_RELEASE_MASTER" ]; then
+            echo "This is a release label!"
+          fi
+
+      # or you can use the action output.
+      # For the label name, use lowercase kebab-case and surround with spaces
+      - run: |
+          scripts/release-major.sh
+        if: contains(steps.pr-labels.outputs.labels, ' release-major ')
+```
+
+## Code Based on the Following
+
+[joerick/pr-labels-action](https://github.com/joerick/pr-labels-action)
+[shioyang/check-pr-labels-on-push-action](https://github.com/shioyang/check-pr-labels-on-push-action)
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { GitHub } from "@actions/github/lib/utils";
+import _deburr from 'lodash/deburr';
+
+async function run() {
+  core.debug("Start");
+
+  const token = core.getInput("github-token", { required: true });
+  const octokit = github.getOctokit(token);
+
+  const labelNames = await getPullRequestLabelNames(octokit);
+  core.debug(`PR label names: ${labelNames}`);
+  const labelsObject: {[k: string]: true} = {}
+
+  for (const label of labelNames)
+  {
+    const identifier = nameToIdentifier(label);
+    const environmentVariable = nameToEnvironmentVariableName(label);
+
+    core.exportVariable(environmentVariable, '1');
+    core.info(`\nFound label ${label}. \n  Setting env var for remaining steps: ${environmentVariable}=1`)
+    labelsObject[identifier] = true
+  }
+
+  const labelsString = ' ' + Object.keys(labelsObject).join(' ') + ' '
+
+  core.info(`\nAction output:\nlabels: ${JSON.stringify(labelsString)}\nlabels-object: ${JSON.stringify(labelsObject)}`)
+  core.setOutput('labels', labelsString);
+  core.setOutput('labels-object', labelsObject);
+}
+
+async function getPullRequestLabelNames(
+  octokit: InstanceType<typeof GitHub>
+): Promise<string[]> {
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  const commit_sha = github.context.sha;
+  core.debug(
+    `PR context - Owner: ${owner} Repo: ${repo} Commit_SHA: ${commit_sha}`
+  );
+
+  const response =
+    await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+      owner,
+      repo,
+      commit_sha,
+    });
+  core.debug(`Retrieved commit data: ${response.data}`);
+
+  const pr = response.data.length > 0 && response.data[0];
+  core.debug(`Retrieved PR: ${pr}`);
+
+  return pr ? pr.labels.map((label) => label.name || "") : [];
+}
+
+function nameToIdentifier(name: string) {
+    return name
+        .replace(/['"“‘”’]+/gu, '')  // remove quotes
+        .replace(/[^\p{Letter}\p{Number}]+/gu, '-')  // non alphanum to dashes
+        .replace(/-+/g, '-')  // remove consecutive dashes
+        .toLowerCase()
+}
+
+function nameToEnvironmentVariableName(name: string) {
+    return 'GITHUB_PR_LABEL_' + (
+        _deburr(name)  // remove accents
+            .replace(/['"“‘”’]+/gu, '')  // remove quotes
+            .replace(/[^\w]+/g, '_')  // non-alphanum to underscores
+            .replace(/_+/g, '_')  // remove consecutive underscores
+            .toUpperCase()
+    )
+}
+
+run().catch((err) => {
+  core.setFailed(`Action failed with error: ${err.message}`);
+});
