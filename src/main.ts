8aa006022440020142cf2cbb9e1ad76bd894facf
Matthew Irby <matt.irby@outlook.com>
2026-01-11 23:15:15 -0500

    chore(action): test if module check broke action run
    Signed-off-by: Matthew Irby <matt.irby@outlook.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as core from "@actions/core";
 import { run } from './index';
 
-if (require.main === module) {
-  run().catch((err) => {
+run().catch((err) => {
     core.setFailed(`Action failed with error: ${err.message}`);
-  });
-}
+});
