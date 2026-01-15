82aad20ee513d28b977b08669f0475088e8ff86f
Matthew Irby <matt.irby@outlook.com>
2026-01-14 20:07:26 -0500

    chore(config): update test config
    Signed-off-by: Matthew Irby <matt.irby@outlook.com>

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    transform: {
-      '^.+\\.(ts|tsx)?$': 'ts-jest',
-      "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', 'main.ts'],
-  };
+  testMatch: ['**.spec.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  testTimeout: 30000,
+  clearMocks: true,
+  testEnvironment: 'node',
+  rootDir: '.',
+  roots: ['<rootDir>'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', 'main.ts'],
+};
